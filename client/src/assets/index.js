48b63189f262218e600f61fe54c52b6795e11186
DESKTOP-8H1UBMN\Mark <markbtesh@gmail.com>
2023-12-18 18:07:42 -0500

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,13 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import painter from './painter.png';
+import painting from './painting.png';
+
+export {
+  download,
+  logo,
+  preview,
+  painter, 
+  painting
+};
