7750278237f838c71302ce04eca7598ed98da9c1
DESKTOP-8H1UBMN\Mark <markbtesh@gmail.com>
2023-12-25 22:07:11 -0500

    4

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://malle-diha.onrender.com/api/v1/post', {
+      const response = await fetch('https://malle-64idoliga-markbtesh.vercel.app/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
