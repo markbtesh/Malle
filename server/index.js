fd319af9c1644f8af2005d0a754dbf31f0239762
markbtesh <63694092+markbtesh@users.noreply.github.com>
2023-12-26 13:32:35 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(cors(
     credentials: true
     }
 ));
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json());
 
 app.use('/api/v1/post', postRoutes);
 app.use ('/api/v1/dalle', dalleRoutes);
