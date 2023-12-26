b68c10de30e42a98f5ce79d79207089b0e87df3a
DESKTOP-8H1UBMN\Mark <markbtesh@gmail.com>
2023-12-26 00:23:03 -0500

    im stupid

--- server/index.js
+++ server/index.js
@@ -11,9 +11,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
-app.use(cors({
-    origin: 'https://malle.markbtesh.com' // or whatever your front-end domain is
-  }));
+
 
 app.use('/api/v1/post', postRoutes);
 app.use ('/api/v1/dalle', dalleRoutes);
