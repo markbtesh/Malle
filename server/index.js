0f3ec1e99b0424afb45c8354432919dd9626254d
markbtesh <63694092+markbtesh@users.noreply.github.com>
2023-12-26 12:25:08 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,6 @@ app.use(cors(
 ));
 app.use(express.json({ limit: '50mb' }));
 
-
 app.use('/api/v1/post', postRoutes);
 app.use ('/api/v1/dalle', dalleRoutes);
 
@@ -30,9 +29,7 @@ const startServer = async () => {
 
     try {
         connectDB(process.env.MONGODB_URL);
-        app.listen(process.env.PORT || 3000, () => {
-            console.log(`Server running on port ${process.env.PORT || 3000}`);
-        });
+        app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
 
      } catch (error)
     {
