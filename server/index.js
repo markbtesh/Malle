8082547e86b1499bd23bc1c119d189c3a7322d18
markbtesh <63694092+markbtesh@users.noreply.github.com>
2023-12-26 12:27:40 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(cors(
     {
-        origin: ["https://malle.vercel.app"],
+        origin: ["https://malle.vercel.app/create-post"],
         method: ["POST", "GET"],
         credentials: true
     }
