f4133badcc92c5e40395c0c9ff21d5c16bc778dc
markbtesh <63694092+markbtesh@users.noreply.github.com>
2023-12-26 23:13:55 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,14 +9,7 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors(
-    {
-        origin: '*',
-    methods: '*',
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-    }
-));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 app.use('/api/v1/post', postRoutes);
